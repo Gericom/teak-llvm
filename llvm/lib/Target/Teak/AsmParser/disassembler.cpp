8f6fc8898c38fca5c3dca8a0b99b7a6017a03ac0
Arisotura <thetotalworm@gmail.com>
2023-08-27 13:16:41 +0200

    add support for ext0/1/2/3 registers

--- llvm/lib/Target/Teak/AsmParser/disassembler.cpp
+++ llvm/lib/Target/Teak/AsmParser/disassembler.cpp
@@ -151,6 +151,16 @@ std::string DsmReg(RegName a) {
         return "st1";
     case RegName::st2:
         return "st2";
+		
+	case RegName::ext0:
+		return "ext0";	
+	case RegName::ext1:
+		return "ext1";	
+	case RegName::ext2:
+		return "ext2";	
+	case RegName::ext3:
+		return "ext3";
+		
     default:
         return "[ERROR]" + std::to_string((int)a);
     }
